c29bd5b5bee6698a74113ee14245a3bb7fde183c
Aishwarya Ganti <aishuganti96@gmail.com>
2020-08-15 15:46:37 +0530

    changes to secret key

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mymusic.db'
-app.secret_key = "flask rocks!"
+app.secret_key = "nptel"
 
 db = SQLAlchemy(app)
